175adc5cf9d9a9463e5e3420cac335a518c466d1
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-02-04 18:45:21 -0300

    organizando pastas

--- Fundamentos/Bloco-4-Introduçao-a-JavaScript-e-Logica-de-Programaçao/4.1-Introduçao-a-JavaScript_Primeiros-passos/scripts.js
+++ Fundamentos/Bloco-4-Introduçao-a-JavaScript-e-Logica-de-Programaçao/4.1-Introduçao-a-JavaScript_Primeiros-passos/scripts.js
@@ -1,154 +1,154 @@
-// >>>>>>>>>> EXERCÍCIO - 1 <<<<<<<<<<<
+>>>>>>>>>> EXERCÍCIO - 1 <<<<<<<<<<<
 
-// const a = 5;
-// const b = 10;
+const a = 5;
+const b = 10;
 
-// console.log(a + b);
-// console.log(a - b);
-// console.log(a * b);
-// console.log(a / b);
-// console.log(a % b);
-
-
-// >>>>>>>>>> EXERCÍCIO - 2 <<<<<<<<<<<
-
-// const c = 42;
-// const d = 22;
-
-// if (c > d) {
-//   console.log(c)
-// } else {
-//   console.log(d)
-// }
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(a % b);
 
 
-// >>>>>>>>>> EXERCÍCIO - 3 <<<<<<<<<<< 
+>>>>>>>>>> EXERCÍCIO - 2 <<<<<<<<<<<
 
-// const numero1 = 84;
-// const numero2 = 32;
-// const numero3 = 18;
+const c = 42;
+const d = 22;
 
-// if (numero1 > numero2 && numero1 > numero3) {
-//     console.log(numero1)
-//   } else if (numero2 > numero1 && numero2 > numero3) {
-//     console.log(numero2)
-//   } else {
-//     console.log(numero3)
-//   }
+if (c > d) {
+  console.log(c)
+} else {
+  console.log(d)
+}
 
 
-// >>>>>>>>>> EXERCÍCIO - 4 <<<<<<<<<<<
-// const valor = 0;
-// if (valor > 0) {
-//   console.log('positive')
-// } else if (valor < 0) {
-//   console.log('negative')
-// }  else {
-//   console.log('zero')
-// }
+>>>>>>>>>> EXERCÍCIO - 3 <<<<<<<<<<< 
+
+const numero1 = 84;
+const numero2 = 32;
+const numero3 = 18;
+
+if (numero1 > numero2 && numero1 > numero3) {
+    console.log(numero1)
+  } else if (numero2 > numero1 && numero2 > numero3) {
+    console.log(numero2)
+  } else {
+    console.log(numero3)
+  }
 
 
-// >>>>>>>>>> EXERCÍCIO - 5 <<<<<<<<<<<
-// const angulo1 = 0;
-// const angulo2 = -70;
-// const angulo3 = 10;
-
-// let anguloTriangulo = (angulo1 + angulo2 + angulo3);
-
-// if (anguloTriangulo === 180) {
-//   console.log(true)
-// } else if (anguloTriangulo > 0 && anguloTriangulo < 180 || anguloTriangulo > 180) {
-//   console.log(false)
-// } else {
-//   console.log('Erro')
-// }
+>>>>>>>>>> EXERCÍCIO - 4 <<<<<<<<<<<
+const valor = 0;
+if (valor > 0) {
+  console.log('positive')
+} else if (valor < 0) {
+  console.log('negative')
+}  else {
+  console.log('zero')
+}
 
 
-// >>>>>>>>>> EXERCÍCIO - 6 <<<<<<<<<<<
+>>>>>>>>>> EXERCÍCIO - 5 <<<<<<<<<<<
+const angulo1 = 0;
+const angulo2 = -70;
+const angulo3 = 10;
 
-// let peçaXadrez = 'CAVALO';
-// let peçaXadrezValida = peçaXadrez.toLowerCase();
+let anguloTriangulo = (angulo1 + angulo2 + angulo3);
 
-// if (peçaXadrezValida === 'rainha') {
-//   console.log('Qualquer direção, quantas casas quiser')
-// } else if(peçaXadrezValida === 'rei') {
-//   console.log('Qualquer direção mas apenas uma casa')
-// } else if (peçaXadrezValida === 'bispo') {
-//   console.log('Diagonal')
-// } else if (peçaXadrezValida === 'torre') {
-//   console.log('Horizontal e Vertical')
-// } else if(peçaXadrezValida === 'cavalo') {
-//   console.log('Em L')
-// } else if(peçaXadrezValida === 'peão') {
-//   console.log('Uma casa pra frente, na primeira jogada pode ir duas porém só come na diagonal')
-// } else {
-//   console.log('Erro, essa peça é inválida!')
-// }
+if (anguloTriangulo === 180) {
+  console.log(true)
+} else if (anguloTriangulo > 0 && anguloTriangulo < 180 || anguloTriangulo > 180) {
+  console.log(false)
+} else {
+  console.log('Erro')
+}
 
 
-// >>>>>>>>>> EXERCÍCIO - 7 <<<<<<<<<<<
+>>>>>>>>>> EXERCÍCIO - 6 <<<<<<<<<<<
 
-// const porcentagem = 100;
+let peçaXadrez = 'CAVALO';
+let peçaXadrezValida = peçaXadrez.toLowerCase();
 
-// if (porcentagem >=90 && porcentagem <= 100) {
-//   console.log('A')
-// } else if (porcentagem < 90 && porcentagem >= 80) {
-//   console.log('B')
-// } else if (porcentagem < 80 && porcentagem >= 70) {
-//   console.log('C')
-// } else if (porcentagem < 70 && porcentagem >= 60) {
-//   console.log('D')
-// } else if (porcentagem < 60 && porcentagem >= 50) {
-//   console.log('E')
-// } else if (porcentagem < 50 && porcentagem >=0) {
-//   console.log('F')
-// } else if (porcentagem < 0 || porcentagem > 100) {
-//   console.log('Erro')
-// } else {
-// }
-
-// >>>>>>>>>> EXERCÍCIO - 8 <<<<<<<<<<<
-
-// const num1 = 11;
-// const num2 = 13;
-// const num3 = 22;
-
-// let resultado = false;
-
-// if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
-//   resultado = true;
-// }
-// console.log(resultado);
+if (peçaXadrezValida === 'rainha') {
+  console.log('Qualquer direção, quantas casas quiser')
+} else if(peçaXadrezValida === 'rei') {
+  console.log('Qualquer direção mas apenas uma casa')
+} else if (peçaXadrezValida === 'bispo') {
+  console.log('Diagonal')
+} else if (peçaXadrezValida === 'torre') {
+  console.log('Horizontal e Vertical')
+} else if(peçaXadrezValida === 'cavalo') {
+  console.log('Em L')
+} else if(peçaXadrezValida === 'peão') {
+  console.log('Uma casa pra frente, na primeira jogada pode ir duas porém só come na diagonal')
+} else {
+  console.log('Erro, essa peça é inválida!')
+}
 
 
-// >>>>>>>>>> EXERCÍCIO - 9 <<<<<<<<<<<
-// const num1 = 98;
-// const num2 = 40;
-// const num3 = 22;
+>>>>>>>>>> EXERCÍCIO - 7 <<<<<<<<<<<
 
-// let resultado = false;
+const porcentagem = 100;
 
-// if (num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0) {
-//   resultado = true;
-// }
-// console.log(resultado);
+if (porcentagem >=90 && porcentagem <= 100) {
+  console.log('A')
+} else if (porcentagem < 90 && porcentagem >= 80) {
+  console.log('B')
+} else if (porcentagem < 80 && porcentagem >= 70) {
+  console.log('C')
+} else if (porcentagem < 70 && porcentagem >= 60) {
+  console.log('D')
+} else if (porcentagem < 60 && porcentagem >= 50) {
+  console.log('E')
+} else if (porcentagem < 50 && porcentagem >=0) {
+  console.log('F')
+} else if (porcentagem < 0 || porcentagem > 100) {
+  console.log('Erro')
+} else {
+}
 
-// >>>>>>>>>> EXERCÍCIO - 10 <<<<<<<<<<<
+>>>>>>>>>> EXERCÍCIO - 8 <<<<<<<<<<<
 
-// const custo = 10;
-// const venda = 15;
+const num1 = 11;
+const num2 = 13;
+const num3 = 22;
 
-// let imposto = (20 / 100 * custo);
-// let custoTotal = (custo + imposto);
-// let lucro = (venda - custoTotal);
+let resultado = false;
 
-// if (custo >= 0 && venda >= 0) {
-//   console.log(lucro * 1000)
-// } else {
-//   console.log('Erro.')
-// }
+if (num1 % 2 === 0 || num2 % 2 === 0 || num3 % 2 === 0) {
+  resultado = true;
+}
+console.log(resultado);
 
-// >>>>>>>>>> EXERCÍCIO - 11 <<<<<<<<<<<
+
+>>>>>>>>>> EXERCÍCIO - 9 <<<<<<<<<<<
+const num1 = 98;
+const num2 = 40;
+const num3 = 22;
+
+let resultado = false;
+
+if (num1 % 2 != 0 || num2 % 2 != 0 || num3 % 2 != 0) {
+  resultado = true;
+}
+console.log(resultado);
+
+>>>>>>>>>> EXERCÍCIO - 10 <<<<<<<<<<<
+
+const custo = 10;
+const venda = 15;
+
+let imposto = (20 / 100 * custo);
+let custoTotal = (custo + imposto);
+let lucro = (venda - custoTotal);
+
+if (custo >= 0 && venda >= 0) {
+  console.log(lucro * 1000)
+} else {
+  console.log('Erro.')
+}
+
+>>>>>>>>>> EXERCÍCIO - 11 <<<<<<<<<<<
 
 let salarioBruto = 3500;
 let aliquotaInss = 0;
