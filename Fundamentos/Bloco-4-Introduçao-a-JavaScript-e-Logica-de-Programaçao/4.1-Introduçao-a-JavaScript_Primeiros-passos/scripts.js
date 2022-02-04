7336c6e064f667eeb1e56d3fcbac60d47e8e471b
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-02-04 15:55:18 -0300

    Feito exercicio 11! :D

--- Fundamentos/Bloco-4-Introduçao-a-JavaScript-e-Logica-de-Programaçao/4.1-Introduçao-a-JavaScript_Primeiros-passos/scripts.js
+++ Fundamentos/Bloco-4-Introduçao-a-JavaScript-e-Logica-de-Programaçao/4.1-Introduçao-a-JavaScript_Primeiros-passos/scripts.js
@@ -150,4 +150,41 @@
 
 // >>>>>>>>>> EXERCÍCIO - 11 <<<<<<<<<<<
 
+let salarioBruto = 3500;
+let aliquotaInss = 0;
+let salarioBase = 0;
+let aliquotaIr = 0;
+let salarioLiquido = 0;
 
+if (salarioBruto <= 1556.94) {
+  aliquotaInss = (8 / 100) * salarioBruto
+    salarioBase = salarioBruto - aliquotaInss
+} else if ( salarioBruto >= 1556.95 && salarioBruto <= 2594.92) {
+  aliquotaInss = (9 / 100) * salarioBruto
+    salarioBase = salarioBruto - aliquotaInss
+} else if ( salarioBruto >= 2594.93 && salarioBruto <= 5189.82) {
+  aliquotaInss = (11 / 100) * salarioBruto
+    salarioBase = salarioBruto - aliquotaInss
+} else {
+  aliquotaInss = (salarioBruto - 570.88)
+  salarioBase = aliquotaInss
+}
+
+if (salarioBase <= 1903.98) {
+  aliquotaIr = 0
+  salarioLiquido = salarioBase
+} else if (salarioBase >= 1903.99 && salarioBase <= 2826.65) {
+  aliquotaIr = (7.5 / 100) * salarioBase - 142.80
+  salarioLiquido = salarioBase - aliquotaIr
+} else if (salarioBruto >= 2826.66 && salarioBase <= 3751.05) {
+  aliquotaIr = (15 / 100) * salarioBase - 354.80
+  salarioLiquido = salarioBase - aliquotaIr
+} else if (salarioBase >= 3751.06 && salarioBase <= 4664.68) {
+  aliquotaIr = (22.5 / 100) * salarioBase - 636.13
+  salarioLiquido = salarioBase - aliquotaIr
+} else {
+  aliquotaIr = (27.5 / 100) * salarioBase - 869.36
+  salarioLiquido = salarioBase - aliquotaIr
+}
+
+console.log(salarioLiquido);
