a817d1d18ce575ec3fbf79d191c8dac668252925
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-02-07 13:53:19 -0300

    Fazendo exercícios bonus, em andamento.

---
+++ Fundamentos/Bloco-4-Introduçao-a-JavaScript-e-Logica-de-Programaçao/4.2-JavaScript-Array-e-loop-For/Exercicios-Bonus/scripts.js
@@ -0,0 +1,14 @@
+let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
+
+for (let index = 1; index < numbers.length; index =+ 1) {
+    for (let index2 = 0; index2 < index; index2 =+1) {
+        if (numbers[index2] > numbers[index]) {
+            let result = numbers[index];
+            numbers[index2] = numbers[index];   
+            numbers[index2] = result;
+        }
+
+    }
+}
+
+console.log(result);
