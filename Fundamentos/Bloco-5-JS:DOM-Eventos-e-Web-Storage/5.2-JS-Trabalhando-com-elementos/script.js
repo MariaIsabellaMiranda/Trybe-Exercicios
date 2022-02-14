8610687a1e472ac0ed2e8ae9ee34391140609387
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-02-13 22:52:19 -0300

    Commit de teste

---
+++ Fundamentos/Bloco-5-JS:DOM-Eventos-e-Web-Storage/5.2-JS-Trabalhando-com-elementos/script.js
@@ -0,0 +1,4 @@
+let ondeVoceEsta = document.querySelector("#elementoOndeVoceEsta");
+ondeVoceEsta.parentElement.style.color = "red"
+ondeVoceEsta.firstElementChild.innerText = "Não estou entendendo nada.";
+document.querySelector("#pai");
