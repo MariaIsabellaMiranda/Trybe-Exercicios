2edbc579c8c5b9a05212a9c2cd6e82cff7c53cd3
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-03-04 15:15:03 -0300

    organizando branch

---
+++ Fundamentos/Bloco-7-Introdução-à-JavaScript-ES6-e-Testes-Unitários/7.1-JS-ES6-let,const,arrow-functions-e-template-literals/script.js
@@ -0,0 +1,21 @@
+// const testingScope = (escopo) => {
+//     if (escopo === true) {
+//       let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//       ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//       console.log(ifScope);
+//     } else {
+//       let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//       console.log(elseScope);
+//     }
+//   }
+
+//   testingScope(true);
+
+//   const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+//   oddsAndEvens.sort((a,b) => a - b);
+
+//   console.log(`Os números ${oddsAndEvens} se encontram de forma crescente!`);
+
+const fatorial = (number) => number > 1 ? number * fatorial(number - 1) : 1;
+console.log(fatorial(5));
