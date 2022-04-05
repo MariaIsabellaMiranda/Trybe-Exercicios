4165710cad983a0346ce6a780529b8698609a227
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-04-05 15:08:27 -0300

    Criados diretorios para Front-End e feitos exercicios do bloco 10.1

---
+++ Front-End/Bloco-10-Introdução-à-React/10.1-Introducao-a-React/hello-world-react/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class MeuNome extends Component {
+  render () {
+    return (
+      <div>
+        <h1>Maria Isabella</h1>
+        <p>Tenho 29 anos, sou casada e tenho duas filhas, meus maiores tesouros!</p>
+      </div>
+    )
+  }
+}
+
+export default Component;
