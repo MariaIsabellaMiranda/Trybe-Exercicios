ee5cb003adbc3a258ec2198b87fd4ebefb3c3f0d
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-04-10 16:03:10 -0300

    Adicionando Pastas

---
+++ Front-End/Bloco-10-Introdução-à-React/10.2-Componentes-React/my-pokedex/src/pokedex.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Pokemon from "./pokemon";
+
+class Pokedex extends React.Component {
+  render() {
+    const pokemons = this.props.poke;
+    return (
+      <div>
+        {pokemons.map((pokemon) => <Pokemon key={pokemon.id} poke={pokemon} />)}
+      </div>
+    );
+  }
+};
+
+export default Pokedex;
