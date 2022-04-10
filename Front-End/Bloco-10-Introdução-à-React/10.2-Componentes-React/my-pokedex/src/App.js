c7d528d8a53a2bc458b834b69ac87f50aeaf1020
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-04-10 15:08:07 -0300

    Exercicios 10.2

---
+++ Front-End/Bloco-10-Introdução-à-React/10.2-Componentes-React/my-pokedex/src/App.js
@@ -0,0 +1,14 @@
+// import './App.css';
+import pokemons from './data';
+import Pokedex from './pokedex';
+
+function App() {
+  return (
+    <>
+      <h1>Pokedex</h1>
+      <Pokedex poke={pokemons} />
+    </>
+  );
+}
+
+export default App;
---
+++ Front-End/Bloco-10-Introdução-à-React/10.2-Componentes-React/my-pokedex/src/pokemon.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+class Pokemon extends React.Component {
+  render() {
+    const { name, type, averageWeight:{value: val}, averageWeight:{measurementUnit: kg}, image } = this.props.poke;
+    return (
+      <>
+        <div>
+          <p>{ name }</p>
+          <p>{ type }</p>
+          <p>{`Average Weight: ${val}${kg}`}</p>
+        </div>
+        <img src={image} alt={name} />
+      </>
+    );
+  }
+}
+
+export default Pokemon;
