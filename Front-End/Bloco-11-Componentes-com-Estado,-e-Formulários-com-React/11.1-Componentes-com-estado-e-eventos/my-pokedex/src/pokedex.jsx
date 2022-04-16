5f4073659771c01375be3dc2b8e74f2d461cd4cd
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-04-16 13:44:52 -0300

    Adicionados botões de filtro e All

--- Front-End/Bloco-11-Componentes-com-Estado,-e-Formulários-com-React/11.1-Componentes-com-estado-e-eventos/my-pokedex/src/pokedex.jsx
+++ Front-End/Bloco-11-Componentes-com-Estado,-e-Formulários-com-React/11.1-Componentes-com-estado-e-eventos/my-pokedex/src/pokedex.jsx
@@ -1,34 +1,66 @@
 import React from "react";
-import Pokemon from "./pokemon";
+import Pokemon from "./Pokemon"
+import Button from "./Button";
+import AllButton from "./AllButton"
 
 class Pokedex extends React.Component {
-  constructor(props) {
-    super(props);
+  constructor() {
+    super();
     this.state = {
-      pokemonIndex: 0, 
+      pokemonIndex: 0,
+      filtro: false,
+      pokemonsFiltrados: [],
     };
     this.chamaOProximo = this.chamaOProximo.bind(this);
-    this.chamaOProximo = this.filtraPoke.bind(this);
+    this.filtraPoke = this.filtraPoke.bind(this);
+    this.allButton = this.allButton.bind(this);
   };
 
   chamaOProximo() {
-    this.setState((prevState, _props) => {
-      pokemonIndex: prevState.pokemonIndex + 1;
-    })
+    const pokemons = this.props.poke;
+    const { pokemonIndex, pokemonsFiltrados, filtro } = this.state;
+    if (filtro) {
+      if(pokemonIndex === pokemonsFiltrados.length -1) {
+        this.setState({pokemonIndex: 0})
+      } else {
+        this.setState((prevState) => ({pokemonIndex: prevState.pokemonIndex + 1}));
+      }
+    } else {
+      if(pokemonIndex === pokemons.length - 1) {
+        this.setState({pokemonIndex: 0})
+      } else {
+        this.setState((prevState) => ({pokemonIndex: prevState.pokemonIndex + 1}));
+      }
+    }
   }
 
-filtraPoke() {
+filtraPoke({ target }) {
   const pokemons = this.props.poke;
-  const pokeFiltrado = pokemons.filter((pokemon, index) => {pokemon[index] === this.state.pokemonIndex})
-  return pokeFiltrado;
+  const tipoPoke = pokemons.filter((pokemon) => pokemon.type === target.name);
+  this.setState({
+    filtro: true,
+    pokemonsFiltrados: tipoPoke,
+  })
+}
+
+allButton() {
+  this.setState({
+    filtro: false,
+  })
 }
 
   render() {
     const pokemons = this.props.poke;
+    const { pokemonIndex, filtro, pokemonsFiltrados } = this.state;
+
     return (
         <div>
-          <button onClick={this.chamaOProximo}>Próximo</button>
-          <Pokemon poke={this.filtraPoke}/> 
+          {filtro ? <Pokemon poke={ pokemonsFiltrados[pokemonIndex] }/> 
+          : <Pokemon poke={ pokemons[pokemonIndex] } />}
+          <button onClick={this.chamaOProximo}>Próximo Pokémon</button>
+          <Button tipo={'Fire'} filtraPoke={this.filtraPoke}/>
+          <Button tipo={'Psychic'} filtraPoke={this.filtraPoke}/>
+          <AllButton allButton={this.allButton}/>
         </div>
     );
   }
