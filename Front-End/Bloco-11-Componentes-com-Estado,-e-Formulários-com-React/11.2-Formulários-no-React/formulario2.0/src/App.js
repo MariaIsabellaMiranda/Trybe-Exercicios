11f25da083d9449c4ad13c85daceaf24b2470b51
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-04-16 15:15:08 -0300

    organizando

---
+++ Front-End/Bloco-11-Componentes-com-Estado,-e-Formulários-com-React/11.2-Formulários-no-React/formulario2.0/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import React from 'react';
+import Fieldset from './Fieldset';
+
+class App extends React.Component {
+  render() {
+    return (
+      <Fieldset />
+    );
+  }
+}
+
+export default App;
