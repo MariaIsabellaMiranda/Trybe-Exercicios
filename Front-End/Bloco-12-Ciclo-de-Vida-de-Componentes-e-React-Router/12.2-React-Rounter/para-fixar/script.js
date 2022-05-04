db1f46f772fe4b96902f0352be941501e1bd2924
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-05-04 14:50:45 -0300

    Criando pastas para bloco 14

---
+++ Front-End/Bloco-12-Ciclo-de-Vida-de-Componentes-e-React-Router/12.2-React-Rounter/para-fixar/script.js
@@ -0,0 +1,9 @@
+function tripleTheChances(chances) {
+  var multiplicandoPor3 = [];
+  for (var index = 0; index < chances.length; index ++) {
+    multiplicandoPor3.push(chances[index]*3);
+  }
+  return multiplicandoPor3;
+}
+
+console.log(tripleTheChances());
