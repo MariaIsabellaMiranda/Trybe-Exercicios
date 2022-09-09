5205b32b0f5b43b4a9e23e5d3d8fc7f9efb5606d
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2022-09-09 18:47:39 -0300

    Treino com enums

---
+++ Back-End/Bloco-26-TypeScript/26.1-Introdução-ao-TypeScript/Para-fixar/index.ts
@@ -0,0 +1,33 @@
+enum diasSemana {
+  segunda = 2,
+  terca,
+  quarta,
+  quinta,
+  sexta,
+  sabado,
+};
+
+const seg: diasSemana = diasSemana.segunda;
+const ter: diasSemana = diasSemana.terca;
+const quar: diasSemana = diasSemana.quarta;
+const quin: diasSemana = diasSemana.quinta;
+const sex: diasSemana = diasSemana.sexta;
+const sab: diasSemana = diasSemana.sabado;
+
+enum coresArcoIris {
+  vermelho = 'vermelho',
+  azul = 'azul',
+  amarelo = 'amarelo',
+  verde = 'verde',
+  violeta = 'violeta',
+  laranja = 'laranja',
+  anil = 'anil'
+};
+
+const ver: coresArcoIris = coresArcoIris.vermelho;
+const azl: coresArcoIris = coresArcoIris.azul;
+const ama: coresArcoIris = coresArcoIris.amarelo;
+const verd: coresArcoIris = coresArcoIris.verde;
+const viol: coresArcoIris = coresArcoIris.violeta;
+const lar: coresArcoIris = coresArcoIris.laranja;
+const anil: coresArcoIris = coresArcoIris.anil;
