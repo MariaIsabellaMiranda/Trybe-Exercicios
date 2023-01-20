d7dd131e800dc06d77fab5c7c8a53add88cfd7e8
Maria Isabella Miranda da Silva <ma_isabella.miranda@hotmail.com>
2023-01-20 15:33:29 -0300

    feat: Exercícios da aula

--- Ciência-da-Computação/Bloco-35-Redes-e-Raspagem-de-Dados/35.2-Raspagem-de-Dados/para_fixar.py
+++ Ciência-da-Computação/Bloco-35-Redes-e-Raspagem-de-Dados/35.2-Raspagem-de-Dados/para_fixar.py
@@ -2,14 +2,16 @@ import requests
 from parsel import Selector
 
 
-response = requests.get("http://books.toscrape.com/")
-selector = Selector(text=response.text)
-titles = selector.css(".product_pod h3 a::attr(title)").getall()
-prices = selector.css(".product_price .price_color::text").getall()
-
-for product in selector.css(".product_pod"):
-    title = product.css("h3 a::attr(title)").get()
-    price = product.css(".price_color::text").get()
-    print(title, price)
-
-print(selector)
+URL_BASE = "http://books.toscrape.com/catalogue/"
+next_page_url = 'page-1.html'
+while next_page_url:
+    # Busca o conteúdo da próxima página
+    response = requests.get(URL_BASE + next_page_url)
+    selector = Selector(text=response.text)
+    # Imprime os produtos de uma determinada página
+    for product in selector.css(".product_pod"):
+        title = product.css("h3 a::attr(title)").get()
+        price = product.css(".price_color::text").get()
+        print(title, price)
+    # Descobre qual é a próxima página
+    next_page_url = selector.css(".next a::attr(href)").get()
